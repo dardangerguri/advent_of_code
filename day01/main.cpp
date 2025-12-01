641a67bfda9e2a558b4a711ce0ad00c155740281
Dardan <dardangerguri@outlook.com>
2025-12-01 15:55:49 +0200

    Day01: Part2 solution

--- day01/main.cpp
+++ day01/main.cpp
@@ -9,6 +9,8 @@ int main() {
 	int zeroCount = 0;
 	char direction;
 	int distance;
+	int loops = 0;
+	int remainder = 0;
 
 	input.open("./input.txt");
 	if (!input) {
@@ -19,17 +21,21 @@ int main() {
 	while (std::getline(input, line)) {
 		direction = line[0];
 		distance = std::stoi(line.substr(1));
+		loops = distance / 100;
+		remainder = distance % 100;
 
-		if (direction == 'R')
-			position += distance;
-		else
-			position -= distance;
-
-		position = (position + 100) % 100;
-
-		if (position == 0)
-			zeroCount++;
-
+		if (direction == 'R') {
+			zeroCount += loops;
+			if (position + remainder >= 100)
+				zeroCount++;
+			position = (position + distance) % 100;
+		}
+		else {
+			zeroCount += loops;
+			if (position > 0 && remainder >= position)
+				zeroCount++;
+			position = (position - (distance % 100) + 100) % 100;
+		}
 	}
 
 	std::cout << "The password is " << zeroCount << std::endl;
