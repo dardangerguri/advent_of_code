289c7cffc0178cf7d74f588ec33401e081a17477
Dardan <dardangerguri@outlook.com>
2025-12-11 00:49:57 +0200

    Day07: Part 2 solution

--- day07/main.cpp
+++ day07/main.cpp
@@ -6,7 +6,13 @@ int main() {
 	std::string line;
 	std::vector<std::string> grid;
 
-	int splitCount = 0;
+	long long totalTimelines = 0;
+	int rows;
+	int cols;
+	int sRow = 0;
+	int sCol = 0;
+	char c;
+	long long currentValue;
 
 	input.open("./input.txt");
 	if (!input) {
@@ -17,30 +23,52 @@ int main() {
 	while (std::getline(input, line))
 		grid.push_back(line);
 
-	for (size_t i = 0; i < grid.size(); i++) {
-		for (size_t j = 0; j < grid[i].size(); j++) {
-			if (i > 0 && grid[i][j] == 'S')
-				continue;
-			if (grid[i - 1][j] == 'S' || grid[i - 1][j] == '|') {
-				if (grid[i][j] == '.' && grid[i - 1][j] == 'S')
-					grid[i][j] = '|';
-				else if (grid[i][j] == '.' && grid[i - 1][j] == '|')
-					grid[i][j] = '|';
-				else if (grid[i][j] == '^' && grid[i - 1][j] == '|') {
-					if (j > 0 && grid[i][j - 1] != '^') {
-						grid[i][j - 1] = '|';
-					}
-					if (j + 1 < grid[i].size() && grid[i][j + 1] != '^') {
-						grid[i][j + 1] = '|';
-					}
-					splitCount++;
-				}
+	rows = grid.size();
+	cols = grid[0].size();
 
+	std::vector<std::vector<long long>> timelineCount(rows, std::vector<long long>(cols, 0));
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			if (grid[i][j] == 'S') {
+				sRow = i;
+				sCol = j;
+				timelineCount[i][j] = 1;
+				break;
 			}
 		}
 	}
 
-	std::cout << "The beam can be split " << splitCount << " times" << std::endl;
+	for (int i = sRow; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			currentValue = timelineCount[i][j];
+			if (currentValue == 0) {
+				continue;
+			}
+
+			c = grid[i][j];
+
+			if (c == 'S' || c == '.' || c == '|') {
+				if (i + 1 < rows) {
+					timelineCount[i + 1][j] += currentValue;
+				}
+			}
+
+			else if (c == '^') {
+				if (j > 0 && i + 1 < rows) {
+					timelineCount[i + 1][j - 1] += currentValue;
+				}
+				if (j + 1 < cols && i + 1 < rows) {
+					timelineCount[i + 1][j + 1] += currentValue;
+				}
+			}
+		}
+	}
+
+	for (int j = 0; j < cols; j++)
+		totalTimelines += timelineCount[rows - 1][j];
+
+	std::cout << "The total timelines are " << totalTimelines << std::endl;
 
 	input.close();
 	return (0);
